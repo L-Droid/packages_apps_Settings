74c47d48f80ca72de17a2adc027bc2af595baeb7
Michael Bestas <mikeioannina@gmail.com>
2014-08-26 16:04:16 +0300

    Settings: Remove leftover from old theme chooser
    Change-Id: Iaf97e2b94c27839eb4f9827a77917fe21a311b4c
    
    Conflicts:
    	src/com/android/settings/Settings.java

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -1283,19 +1283,6 @@ public class Settings extends PreferenceActivity
             revert = true;
         }
 
-        // a temp hack while we prepare to switch
-        // to the new theme chooser.
-        if (header.id == R.id.theme_chooser) {
-            try {
-                Intent intent = new Intent();
-                intent.setClassName("com.tmobile.themechooser", "com.tmobile.themechooser.ThemeChooser");
-                startActivity(intent);
-                return;
-            } catch(ActivityNotFoundException e) {
-                 // Do nothing, we will launch the submenu
-            }
-        }
-
         super.onHeaderClick(header, position);
 
         if (revert && mLastHeader != null) {
