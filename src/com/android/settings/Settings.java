d1ac8608470287593cd21a5e2eb1754ba923b317
SeniorLimpio <seniorlimpio1@gmail.com>
2014-10-08 07:43:44 +1300

    Fix Settings

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -91,6 +91,7 @@ import com.android.settings.ldroid.lockscreen.LockscreenInterface;
 import com.android.settings.ldroid.ButtonSettings;
 import com.android.settings.ldroid.StatusBar;
 import com.android.settings.ldroid.NotificationDrawer;
+import com.android.settings.ldroid.fragments.NetworkTraffic;
 import com.android.settings.location.LocationEnabler;
 import com.android.settings.location.LocationSettings;
 import com.android.settings.net.MobileDataEnabler;
